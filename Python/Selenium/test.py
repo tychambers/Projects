48518849176e4f1880dea97f865256c214893692
tychambers <137524458+tychambers@users.noreply.github.com>
2024-09-29 16:39:30 -0500

    Add files via upload

---
+++ Python/Selenium/test.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+song_name = input("What is the name of the song?: ")
+band_name = input("What is the name of the band? (If you do not know leave blank): ")
+
+if len(band_name) <= 1:
+    search_text = f"{song_name} lyrics"
+else:
+    search_text = f"{song_name} by {band_name} lyrics"
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+
+# commented below as it stopped working replaced with the line below it
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://www.google.com/")
+driver.maximize_window()
+
+search_bar = driver.find_element(By.XPATH, '//*[@id="APjFqb"]')
+search_bar.send_keys(search_text)
+search_bar.send_keys(Keys.RETURN)
+
+try:
+    song_title = driver.find_element(By.XPATH, "//div[@class = 'PZPZlf ssJ7i B5dxMb']")
+    artist_name = driver.find_element(By.XPATH, "//div[@class = 'iAIpCb PZPZlf']")
+    lyrics = driver.find_element(By.XPATH, "//div[@jsname = 'WbKHeb']")
+    print(song_title.text + "\n")
+    print(artist_name.text + "\n")
+    print(lyrics.text)
+except NoSuchElementException:
+    print("We could not find lyrics to that song, please try again.")
